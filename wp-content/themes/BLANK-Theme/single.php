d3658c09cb9c2c0f8b780137c01d2e66788e4d32
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-25 19:41:34 +0530

    Create single.php for single blog post entries.

--- wp-content/themes/BLANK-Theme/single.php
+++ wp-content/themes/BLANK-Theme/single.php
@@ -1,14 +1,12 @@
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
+
+<h2 id="post-title"><?php the_title(); ?></h2>
+
+<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
 
 <div id="main-content">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			
-			<h2><?php the_title(); ?></h2>
-			
-			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
 
 			<div class="entry">
 				
@@ -25,8 +23,6 @@
 		</div>
 
 	<?php // comments_template(); ?>
-
-	<?php endwhile; endif; ?>
 	
 </div>
 	
